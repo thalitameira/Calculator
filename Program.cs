365c0d87e4ec877393fce6a3a5422c561410c97c
thalitameira <117841322+thalitameira@users.noreply.github.com>
2023-05-09 15:37:13 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+
+            Console.WriteLine("=============================");
+
+            Console.WriteLine("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                default: Menu(); break;
+
+            }
+
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            double resultado = n1 + n2;
+
+            //Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine($"O resultado da soma é: {resultado} ");
+            //Console.WriteLine($"O resultado da soma é: {n1 + n2} ");
+            //Console.WriteLine("O resultado da soma é: " + (n1 + n2));
+
+            Console.ReadKey();
+            Menu();
+
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            double resultado = n1 - n2;
+
+            //Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine($"O resultado da subtração é: {resultado} ");
+            //Console.WriteLine($"O resultado da soma é: {n1 + n2} ");
+            //Console.WriteLine("O resultado da soma é: " + (n1 + n2));
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            double resultado = n1 / n2;
+
+            //Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine($"O resultado da divisão é: {resultado} ");
+            //Console.WriteLine($"O resultado da soma é: {n1 + n2} ");
+            //Console.WriteLine("O resultado da soma é: " + (n1 + n2));
+
+            Console.ReadKey();
+            Menu();
+
+        }
+
+        static void Multiplicacao()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            double resultado = n1 * n2;
+
+            //Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine($"O resultado da multiplicação é: {resultado} ");
+            //Console.WriteLine($"O resultado da soma é: {n1 + n2} ");
+            //Console.WriteLine("O resultado da soma é: " + (n1 + n2));
+
+            Console.ReadKey();
+            Menu();
+        }
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculator
+ Uma calculadora simples feita em C# usando funções.
